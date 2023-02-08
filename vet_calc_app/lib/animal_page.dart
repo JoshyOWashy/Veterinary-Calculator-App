7e818d5c0a514c1bec9b361f23887c8bf0495d71
Josh Minyard <joshdaminyard@gmail.com>
2023-02-07 22:24:29 -0800

    seperated pages into different files

---
+++ vet_calc_app/lib/animal_page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'animals.dart';
+import 'main.dart';
+
+// Main page
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    // debugPrint(appState.curAnimal);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(appName,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          ),
+          const SizedBox(height: 25),
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            // EQUINE BUTTON
+            SizedBox(
+              width: 150,
+              height: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  appState.chooseAnimal(Animal.equine);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DrugPage()),
+                  );
+                },
+                child: Text(animalToString(Animal.equine)),
+              ),
+            ),
+
+            const SizedBox(width: 25),
+
+            // SHEEP GOAT BUTTON
+            SizedBox(
+              width: 150,
+              height: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  appState.chooseAnimal(Animal.sheepGoat);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DrugPage()),
+                  );
+                },
+                child: Text(animalToString(Animal.sheepGoat)),
+              ),
+            )
+          ]),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // CAMELID BUTTON
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.chooseAnimal(Animal.camelid);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DrugPage()),
+                    );
+                  },
+                  child: Text(animalToString(Animal.camelid)),
+                ),
+              ),
+
+              const SizedBox(width: 25),
+
+              // SWINE BUTTON
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.chooseAnimal(Animal.swine);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DrugPage()),
+                    );
+                  },
+                  child: Text(animalToString(Animal.swine)),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // CATTLE BUTTON
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.chooseAnimal(Animal.cattle);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SecondRoute()),
+                    );
+                  },
+                  child: Text(animalToString(Animal.cattle)),
+                ),
+              ),
+
+              const SizedBox(width: 25),
+
+              // DOG BUTTON
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.chooseAnimal(Animal.dog);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DrugPage()),
+                    );
+                  },
+                  child: Text(animalToString(Animal.dog)),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // CAT BUTTON
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.chooseAnimal(Animal.cat);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DrugPage()),
+                    );
+                  },
+                  child: Text(animalToString(Animal.cat)),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+}
---
+++ vet_calc_app/lib/weight_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'main.dart';
+
+class WeightForm extends StatefulWidget {
+  const WeightForm({super.key});
+
+  @override
+  WeightFormState createState() {
+    return WeightFormState();
+  }
+}
+
+// Weight Page
+class WeightFormState extends State<WeightForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    var animal = appState.curAnimal;
+    var drug = appState.curDrug;
+
+    final textController = TextEditingController();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Weight of $animal  ($drug)'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Center(
+              //add some padding
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                TextFormField(
+                  controller: textController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'(^\d*\.?\d*)'))
+                  ],
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'Enter Weight for $animal in kg',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a weight';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          appState.chooseWeight(textController.text);
+                          debugPrint(
+                              "Weight ${textController.text} in WeightPage");
+                          debugPrint("Animal $animal in WeightPage");
+                          debugPrint("Drug $drug in WeightPage");
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('yay')),
+                          );
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(builder: (context) => const DosagePage()),
+                          // );
+                        }
+                      },
+                      child: const Text("Calculate")),
+                )
+              ])),
+        ));
+  }
+}
