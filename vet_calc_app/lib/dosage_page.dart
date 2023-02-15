b215108105b54721c96c166dd970889a980f7dac
NawafAlOthman <72106212+NawafAlOthman@users.noreply.github.com>
2023-02-15 14:44:54 -0800

    Able to access drug information after button click

--- vet_calc_app/lib/dosage_page.dart
+++ vet_calc_app/lib/dosage_page.dart
@@ -6,13 +6,13 @@ import 'main.dart';
 class DosagePage extends StatelessWidget {
   const DosagePage({super.key});
 
-  Object databaseQuery() async {
+  Future<Object?> databaseQuery() async {
     DatabaseReference ref =
         FirebaseDatabase.instance.ref("Species/Cattle/Drugs");
 
     DatabaseEvent event = await ref.once();
-    debugPrint("Snapshot type: ${event.type}");
-    debugPrint("Snapshot: ${event.snapshot.value}");
+    //debugPrint("Snapshot type: ${event.type}");
+    //debugPrint("Snapshot: ${event.snapshot.value}");
 
     //shows the data here
 
@@ -22,22 +22,49 @@ class DosagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    // debugPrint(appState.curAnimal);
-
     var animal = appState.curAnimal;
-    Object event = databaseQuery();
-
-    debugPrint("Snapshot 2: $event");
-
-    //shows "Instance of 'Future<dynamic>'" here instead of the actual data
+    var drugname = appState.curDrug;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Drugs for $animal'),
-        ),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, children: const []),
-        ));
+      appBar: AppBar(
+        title: Text('Drugs for $animal'),
+      ),
+      body: FutureBuilder(
+        future: databaseQuery(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // data has been loaded, build the widget tree
+            var data = snapshot.data as List<dynamic>;
+
+            //debugPrint("Snapshot 2: $data");
+            var main_drug;
+            for (var drug in data) {
+              // debugPrint("Snapshot 3: $drug");
+              if (drug['Name'] == drugname) {
+                main_drug = drug;
+              }
+            }
+            debugPrint("Snapshot 4: $main_drug");
+
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            // an error occurred while loading the data
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else {
+            // data is still loading
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
