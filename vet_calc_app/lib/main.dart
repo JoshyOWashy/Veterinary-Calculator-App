2d58c2b309a4f14a1c705cd95927ddd525589dad
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2023-03-07 17:13:56 -0800

    move files to folders

--- vet_calc_app/lib/main.dart
+++ vet_calc_app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:vet_calc_app/animal_page.dart';
-import 'animals.dart';
+import 'package:vet_calc_app/ui/animal_page.dart';
+import 'data/animals.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'data/firebase_options.dart';
 
 const appName = "Vet RX Calculator";
 
